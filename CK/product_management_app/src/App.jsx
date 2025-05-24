6cdaa2e37f8edef1948df1bff04f2fa029a16773
QuocBao <nbao76296@gmail.com>
2025-05-24 15:35:55 +0700

    1. Trang chi tiet

--- CK/product_management_app/src/App.jsx
+++ CK/product_management_app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 
 const App = () => {
   return (
@@ -8,10 +9,11 @@ const App = () => {
       <div className="container mx-auto">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
         </Routes>
       </div>
     </Router>
   );
 };
 
-export default App; 
+export default App;
